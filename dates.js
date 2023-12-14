b039daec06ed7e0d8356c0c2137bbeecf8a2c29d
Frontend Jedi <60159086+Kessyblaise1@users.noreply.github.com>
2023-12-14 08:40:46 +0100

    added fluvius

--- dates.js
+++ dates.js
@@ -1,3 +1,5 @@
 const boa_wells = new Date("2023-12-05");
 const wellsfargo = new Date("2023-11-26");
 const t_mobi = new Date("2023-12-14");
+const fluvius = new Date("2023-11-22");
+const fluvius_rec = new Date("2023-12-14");
