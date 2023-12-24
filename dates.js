1827849ad5ef117d08eab26c66101fb5a6fcec42
Frontend Jedi <60159086+Kessyblaise1@users.noreply.github.com>
2023-12-24 23:16:57 +0100

    sleep no good

--- dates.js
+++ dates.js
@@ -1,6 +1,7 @@
 const boa_wells = new Date("2023-12-05");
 const wellsfargo = new Date("2023-11-26");
-const t_mobi = new Date("2023-12-14");
+const t_mobi = new Date("2023-11-14");
+const t_mobi_ssn = new Date("2023-12-14");
 const fluvius = new Date("2023-11-22");
 const fluvius_rec = new Date("2023-12-14");
 const santana = new Date("2023-12-15");
