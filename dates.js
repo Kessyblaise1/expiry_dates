c7c9d3019577b1bce4f5755ff1aeb2c090bc3b4a
Frontend Jedi <60159086+Kessyblaise1@users.noreply.github.com>
2024-01-16 15:22:45 +0100

    added regions

--- dates.js
+++ dates.js
@@ -9,3 +9,4 @@ const aasu_login = new Date("2023-12-28");
 const nafe_rehost_date = new Date("2024-01-05");
 const nafe_date = new Date("2023-01-05");
 const usaa_wells = new Date("2024-01-10");
+const regions_date = new Date("2024-01-16")
